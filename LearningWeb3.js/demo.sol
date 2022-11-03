38d3967aa08c75da12855f84806d3491517345d8
Lalit Thapa <lalithapaji@gmail.com>
2022-11-03 16:49:37 +0530

    Interaction with the browser

---
+++ LearningWeb3.js/demo.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract demo{
+    uint256 public x=10;
+    function set(uint256 _x)public{
+        x=_x;
+    }
+}
+//This will be used in learning Web3.js
